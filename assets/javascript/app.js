02c12f6f2d04014dc1533774f218b69ff830d4ac
murnda <mdelong333@gmail.com>
2019-11-03 15:18:02 -0500

    added javscript to update active status on links

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
     $(".sidenav").sidenav();
 })
+
+var linkContainer = document.getElementById("my-nav");
+
+var links = linkContainer.getElementsByClassName("nav-link");
+
+for (var i = 0; i <links.length; i++) {
+    links[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+};
